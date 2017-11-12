a394aa702003bbbd9992b158bbedd4500bd7c151
alenadmit <dmitalyona@yandex.ru>
2017-11-12 23:35:25 +0300

    weather and styles added

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const data = {
 };
 
 const getPlugin = (data, dist) => {
+    const token = "c6898c020dd0d3c6977019766daf9eca";
+    const url_curr = "http://api.openweathermap.org/data/2.5/weather?q={HOLDER}&appid=" + token;
+
     const makeElement = (type='div', attributes={}, content='') => {
         let element = document.createElement(type);
         element.innerHTML = content;
@@ -30,7 +33,25 @@ const getPlugin = (data, dist) => {
         for (let i = 0; i < data.cities.length; i++) {
             let item = makeElement('li', {
                 class: 'list__item'
+            });
+
+            let itemName = makeElement('div', {
+                class: 'list__item__item-name'
             }, data.cities[i]);
+
+            let itemWeather = makeElement('div', {
+                class: 'list__item__item-weather'
+            });
+
+            const url = url_curr.replace("{HOLDER}", data.cities[i]);
+
+            fetch(url)
+                .then(res => res.json())
+                .then(res => itemWeather.innerHTML = Math.floor(res.main.temp) - 273 + "° now in this city")
+                .catch(e => console.log(e));
+
+            item.appendChild(itemName);
+            item.appendChild(itemWeather);
             list.appendChild(item);
         }
     })();
