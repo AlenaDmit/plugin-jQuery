5f68aaeea663da9458618f1f9dbfb3976f760e8a
alenadmit <dmitalyona@yandex.ru>
2017-11-19 19:24:39 +0300

    fixed interface, added removing

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const data = {
     direction: 'up'
 };
 
-const getPlugin = (data, dist) => {
+function getPlugin(cities, dist, direction) {
+    const data = cities;
+    const sss = cities;
+    const ddd = dist;
     const token = "c6898c020dd0d3c6977019766daf9eca";
     const url_curr = "http://api.openweathermap.org/data/2.5/weather?q={HOLDER}&appid=" + token;
 
     const makeElement = (type='div', attributes={}, content='') => {
         let element = document.createElement(type);
         element.innerHTML = content;
-        for (key in attributes) {
+        for (const key in attributes) {
             element.setAttribute(key, attributes[key])
         }
         return element;
@@ -29,35 +32,65 @@ const getPlugin = (data, dist) => {
     dist.append(fragment);
     $('body').append(dist);
 
-    const getListItem = (() => {
-        for (let i = 0; i < data.cities.length; i++) {
-            let item = makeElement('li', {
-                class: 'list__item'
-            });
-
-            let itemName = makeElement('div', {
-                class: 'list__item__item-name'
-            }, data.cities[i]);
-
-            let itemWeather = makeElement('div', {
-                class: 'list__item__item-weather'
-            });
-
-            const url = url_curr.replace("{HOLDER}", data.cities[i]);
-
-            fetch(url)
-                .then(res => res.json())
-                .then(res => itemWeather.innerHTML = Math.floor(res.main.temp) - 273 + "° now in this city")
-                .catch(e => console.log(e));
-
-            item.append(itemName);
-            item.append(itemWeather);
-            list.append(item);
+    const getCityWeatherPromise = (city) => fetch(url_curr.replace("{HOLDER}", city), {
+        method: 'post',
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
         }
-    })();
-};
+    });
+
+    const renderOneElement = (elementDist) => (city) => {
+        let item = makeElement('li', {
+            class: 'list__item'
+        });
+
+        let itemName = makeElement('div', {
+            class: 'list__item__item-name'
+        }, city);
+
+        let itemWeather = makeElement('div', {
+            class: 'list__item__item-weather'
+        });
+        renderPromiseWeatherToList(city, itemWeather);
+        item.append(itemName);
+        item.append(itemWeather);
+        elementDist.append(item);
+    };
+
+    function renderElements(data = sss, dist = list) {
+        data.cities.forEach(renderOneElement(dist));
+        const items = $('.list__item');
+        items.click(function (e) {
+            if (e.currentTarget.classList.contains('active-item')) return;
+            items.removeClass('active-item');
+            items.removeAttr('style');
+            e.currentTarget.classList.add('active-item');
+            // $(e.currentTarget).animate({
+            //     width: "70%",         // ширина станет 70%
+            //     opacity: 0.4,         // прозрачность будет 40%
+            //     marginLeft: "0.6in",  // отступ от левого края элемента станет равным 6 дюймам
+            //     fontSize: "3em",      // размер шрифта увеличится в 3 раза
+            //     borderWidth: "10px"   // толщина рамки станет 10 пикселей
+            // }, 1500);
+        });
+    }
+
+    const renderPromiseWeatherToList = (cityName, dist) => {
+        const weather = getCityWeatherPromise(cityName);
+        weather.then(r => r.json()).then(renderWeatherToItem(dist));
+    };
+
+    const renderWeatherToItem = (dist) => (response) => {
+        dist.innerHTML = Math.floor(response.main.temp) - 273 + "° now in this city"; // itemWeather
+    };
+
+    return {
+        renderElements,
+    };
+}
 
 $(document).ready(function () {
     const block = $('.block');
-    getPlugin(data, block);
+    const plugin = getPlugin(data, block);
+    plugin.renderElements()
 });
